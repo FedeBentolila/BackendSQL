37eda0c4f78315a4bcd7a57d82f9fdc5d46afb9d
FedeBentolila <federico_bentolila@hotmail.com>
2022-11-28 09:36:25 -0300

    sql3

--- Websockets/server.js
+++ Websockets/server.js
@@ -1,129 +1,116 @@
-var knex = require('knex')({
-  client: 'mysql',
+var knex = require("knex")({
+  client: "mysql",
   connection: {
-    host: '127.0.0.1',
-    user: 'root',
-    password: '',
-    database: 'productos'
+    host: "127.0.0.1",
+    user: "root",
+    password: "",
+    database: "productos",
   },
-  pool: { min: 0, max: 7 }
+  pool: { min: 0, max: 7 },
 });
 
-knex.schema.hasTable('productos')
-.then((existe)=>{
+knex.schema.hasTable("productos").then((existe) => {
   if (!existe) {
-    knex.schema.createTable('productos', (table) => {
-      table.increments('id')
-      table.string('title')
-      table.string('thumbnail')
-      table.integer('price')
-    })
-    .then(() => console.log("tabla creada"))
-    .finally(()=>{
-      knex.destroy();
-    }
-    )
-  
+    knex.schema
+      .createTable("productos", (table) => {
+        table.increments("id");
+        table.string("title");
+        table.string("thumbnail");
+        table.integer("price");
+      })
+      .then(() => console.log("tabla creada"))
+      .finally(() => {
+        knex.destroy();
+      });
   }
-
-})
-
-var knex2 = require('knex')({
-  client: 'sqlite3',
-  connection: {
-    filename:"./DB/mensajes.sqlite"
-  },
-  useNullAsDefault: true
 });
 
-knex2.schema.hasTable('mensajes')
-.then((existe)=>{
+var knex2 = require("knex")({
+  client: "sqlite3",
+  connection: {
+    filename: "./DB/mensajes.sqlite",
+  },
+  useNullAsDefault: true,
+});
+
+knex2.schema.hasTable("mensajes").then((existe) => {
   if (!existe) {
-    knex2.schema.createTable('mensajes', (table) => {
-      table.string('author')
-      table.string('text')
-      table.string('time')
-    })
-    .then(() => console.log("tabla creada"))
-    .finally(()=>{
-      knex2.destroy();
-    }
-    )
-  
+    knex2.schema
+      .createTable("mensajes", (table) => {
+        table.string("author");
+        table.string("text");
+        table.string("time");
+      })
+      .then(() => console.log("tabla creada"))
+      .finally(() => {
+        knex2.destroy();
+      });
   }
-
-})
-
-
-
+});
 
 let Arreglodeproductos = [];
 
-
 const express = require("express");
-const {Server: HttpServer}= require('http');
+const { Server: HttpServer } = require("http");
 
-const {Server: IOServer}= require('socket.io');
+const { Server: IOServer } = require("socket.io");
 const aplicacion = express();
-const httpServer= new HttpServer(aplicacion);
-const io= new IOServer(httpServer);
+const httpServer = new HttpServer(aplicacion);
+const io = new IOServer(httpServer);
 
 const PUERTO = 8080;
 
 httpServer.listen(PUERTO, () => {
-  console.log(
-    `Aplicación escuchando en el puerto: ${PUERTO}`
-  );
+  console.log(`Aplicación escuchando en el puerto: ${PUERTO}`);
 });
 
-let messages = [
-];
+let messages = [];
 
-
-const publicRoot= "./public"
+const publicRoot = "./public";
 
 aplicacion.use(express.static(publicRoot));
 
+io.on("connection", function (socket) {
+  console.log("Un cliente se ha conectado");
 
-io.on('connection', function(socket){
-  console.log('Un cliente se ha conectado');
- 
-  getAllmensajessqlite().then(()=>{
-    socket.emit('messages', messages);
-  }).then(()=>{
-    messages=[];
-  })
+  getAllmensajessqlite()
+    .then(() => {
+      socket.emit("messages", messages);
+    })
+    .then(() => {
+      messages = [];
+    });
 
-  getAllmysql().then(()=>{
-    socket.emit('lineaproducto', Arreglodeproductos);
-  }).then(()=>{
-    Arreglodeproductos=[];
-  })
+  getAllmysql()
+    .then(() => {
+      socket.emit("lineaproducto", Arreglodeproductos);
+    })
+    .then(() => {
+      Arreglodeproductos = [];
+    });
 
-
-  socket.on('new message', data=>{
-     savemensajessqlite(data).then(()=>{
-      getAllmensajessqlite().then(()=>{
-        io.sockets.emit('messages', messages)
-      }).then(()=>{
-        messages=[];
-      })
-
-     })
-        
+  socket.on("new message", (data) => {
+    savemensajessqlite(data).then(() => {
+      getAllmensajessqlite()
+        .then(() => {
+          io.sockets.emit("messages", messages);
+        })
+        .then(() => {
+          messages = [];
+        });
+    });
   });
 
-  socket.on('new lineaproducto', data=>{
-
-  savemysql(data).then(()=>{
-    getAllmysql().then(()=>{
-      socket.emit('lineaproducto2', Arreglodeproductos);
-    }).then(()=>{
-      Arreglodeproductos=[];
-    })
-
-  })
-
+  socket.on("new lineaproducto", (data) => {
+    savemysql(data).then(() => {
+      getAllmysql()
+        .then(() => {
+          socket.emit("lineaproducto2", Arreglodeproductos);
+        })
+        .then(() => {
+          Arreglodeproductos = [];
+        });
+    });
   });
 });
 
@@ -132,66 +119,72 @@ aplicacion.use(express.json());
 aplicacion.use(express.urlencoded({ extended: true }));
 
 aplicacion.get("/", (peticion, respuesta) => {
-  respuesta.sendFile('index.html', {root: publicRoot});
+  respuesta.sendFile("index.html", { root: publicRoot });
 });
 
 aplicacion.get("/chat", (peticion, respuesta) => {
-  respuesta.sendFile('chat.html', {root: publicRoot});
+  respuesta.sendFile("chat.html", { root: publicRoot });
 });
-
 
 ////Funciones para  mensajes
 
+async function savemensajessqlite(objeto) {
+  await knex2("mensajes")
+    .insert(objeto)
+    .then(() => console.log("mensaje insertado"))
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+}
 
-async function savemensajessqlite(objeto){
-  await knex2('mensajes').insert(objeto)
-   .then(()=> console.log("mensaje insertado"))
-   .catch((err)=>{console.log(err); throw err})
-   
- }
+async function getAllmensajessqlite() {
+  await knex2
+    .from("mensajes")
+    .select("*")
+    .then((rows) => {
+      for (row of rows) {
+        let objeto = { author: row.author, text: row.text, time: row.time };
 
-async function getAllmensajessqlite(){
-  
-  await knex2.from('mensajes').select("*")
-  .then((rows)=>{
-    for (row of rows) {
-      let objeto ={author:row.author, text:row.text, time: row.time}
-
-      messages.push(objeto)
-    }
-
-  })
-  .catch((err)=>{console.log(err); throw err})
-  
-  
-
+        messages.push(objeto);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
 }
 
 /// Funciones para productos
 
-async function savemysql(objeto){
- await knex('productos').insert(objeto)
-  .then(()=> console.log("producto insertado"))
-  .catch((err)=>{console.log(err); throw err})
-  
+async function savemysql(objeto) {
+  await knex("productos")
+    .insert(objeto)
+    .then(() => console.log("producto insertado"))
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
 }
 
+async function getAllmysql() {
+  await knex
+    .from("productos")
+    .select("*")
+    .then((rows) => {
+      for (row of rows) {
+        let objeto = {
+          id: row.id,
+          title: row.title,
+          price: row.price,
+          thumbnail: row.thumbnail,
+        };
 
-async function getAllmysql(){
-  
-  await knex.from('productos').select("*")
-  .then((rows)=>{
-    for (row of rows) {
-      let objeto ={id:row.id, title:row.title, price: row.price, thumbnail: row.thumbnail}
-
-      Arreglodeproductos.push(objeto)
-    }
-
-  })
-  .catch((err)=>{console.log(err); throw err})
-  
+        Arreglodeproductos.push(objeto);
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
 }
-
-
-
-
